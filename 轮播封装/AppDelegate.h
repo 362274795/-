893a7952403fdd33e9058282495a8aeea4b317f6
lc_nicky <362274795@qq.com>
2016-07-26 11:42:36 +0800

    Initial Commit

---
+++ 轮播封装/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  轮播封装
+//
+//  Created by lc on 16/7/26.
+//  Copyright © 2016年 lcnicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
